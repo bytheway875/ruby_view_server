4b66dee72cc429a158d37931a502e1e9ea255185
MakerSquare Student <student@lab-2.att.net>
2013-06-25 15:04:18 -0500

    Added erb code

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,14 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+y = %w{this is an array}
+template1 = ERB.new "<%= y %>"
+puts template1.result(binding)
+
+z = {:key => "value"}
+template2 = ERB.new "<%= z%>"
+puts template2.result(binding)
+
+
+puts template.result(binding)
